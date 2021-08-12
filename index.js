2e873c408fa12db85a056e592498cfa14586a632
Rushwayc <rushwayc@yahoo.com>
2021-08-12 11:25:06 +0200

    Created Delete button
    Added a delete button that deletes / resets the list. In the future need to add functionality to delete specific items.

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 let textToSave = document.getElementById("textToSave")
 let saveButton = document.getElementById("saveButton")
+let deleteButton = document.getElementById("deleteButton")
 let savedList = document.getElementById("savedList")
 let list = []
+const fromLocalStorage = JSON.parse(localStorage.getItem("list"))
+
+if (fromLocalStorage) {
+    list = fromLocalStorage
+    renderList()
+}
 
 saveButton.addEventListener("click", function() {
     list.push(textToSave.value)
     textToSave.value = ""
+    localStorage.setItem("list", JSON.stringify(list))
+    renderList()
+})
+
+deleteButton.addEventListener("click", function() {
+    localStorage.clear()
+    list = []
     renderList()
 })
 
